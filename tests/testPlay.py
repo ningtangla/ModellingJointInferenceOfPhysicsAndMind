f88a51cbebce714604152c6c306af29864742106
Jianzhi <ljzprivate@yahoo.com>
2019-06-19 19:02:37 -0700

    Added play.py and its test, testPlay.py

---
+++ tests/testPlay.py
@@ -0,0 +1,52 @@
+import sys
+sys.path.append("..")
+import unittest
+from ddt import ddt, data, unpack
+import src.play as play
+import src.simple1DEnv as env
+import exec.generateTrainingDataForNN as generateData
+
+
+@ddt
+class TestPlay(unittest.TestCase):
+    def setUp(self):
+        bound_low = 0
+        bound_high = 7
+        self.transition = env.TransitionFunction(bound_low, bound_high)
+        self.target_state = bound_high
+        self.isTerminal = env.Terminal(self.target_state)
+        self.reset = lambda: 0
+
+    @data(([0], [(0, 0)]*5),
+          ([1, -1], [(0, 1), (1, -1), (0, 1), (1, -1), (0, 1)]),
+          ([1, 1, 1, 1, 1], [(0, 1), (1, 1), (2, 1), (3, 1), (4, 1)]),
+          ([7], [(0, 7), (7, None)]),
+          ([2, 0, 3, 0, 0, 2], [(0, 2), (2, 3), (5, 2), (7, None)]))
+    @unpack
+    def testSampleTrajectory(self, policyArray, groundTruthTraj):
+        maxRunningSteps = 5
+        sampleTraj = play.SampleTrajectory(maxRunningSteps, self.transition, self.isTerminal, self.reset)
+        policy = lambda i: policyArray[i]
+        traj = sampleTraj(policy)
+        self.assertEqual(traj, groundTruthTraj)
+
+    @data(([{0: 1}], [(0, 0, {0: 1})]*5),
+          ([{1: 0.7, 2: 0.1, 3: 0.2}, {-1: 0.9, 0: 0.1}],
+           [(0, 1, {1: 0.7, 2: 0.1, 3: 0.2}), (1, -1, {-1: 0.9, 0: 0.1}), (0, 1, {1: 0.7, 2: 0.1, 3: 0.2}), (1, -1, {-1: 0.9, 0: 0.1}), (0, 1, {1: 0.7, 2: 0.1, 3: 0.2})]),
+          ([{1: 0.6, -1: 0.4}]*5,
+           [(0, 1, {1: 0.6, -1: 0.4}), (1, 1, {1: 0.6, -1: 0.4}), (2, 1, {1: 0.6, -1: 0.4}), (3, 1, {1: 0.6, -1: 0.4}), (4, 1, {1: 0.6, -1: 0.4})]),
+          ([{7: 0.7, 10: 0.3}], [(0, 7, {7: 0.7, 10: 0.3}), (7, None, None)]),
+          ([{2: 0.9, -2: 0.1}, {0: 1}, {3: 0.4, 2: 0.2, 1: 0.2}, {0: 1}, {0: 1}, {2: 0.99, 4: 0.01}],
+           [(0, 2, {2: 0.9, -2: 0.1}), (2, 3, {3: 0.4, 2: 0.2, 1: 0.2}), (5, 2, {2: 0.99, 4: 0.01}), (7, None, None)]))
+    @unpack
+    def testSampleTrajectoryWithActionDist(self, policyArray, groundTruthTraj):
+        maxRunningSteps = 5
+        distToAction = generateData.distToGreedyAction
+        sampleTraj = play.SampleTrajectoryWithActionDist(maxRunningSteps, self.transition, self.isTerminal, self.reset, distToAction)
+        policy = lambda i: policyArray[i]
+        traj = sampleTraj(policy)
+        self.assertEqual(traj, groundTruthTraj)
+
+
+if __name__ == "__main__":
+    unittest.main()
