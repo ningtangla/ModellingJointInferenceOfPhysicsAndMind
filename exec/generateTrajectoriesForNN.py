3e7bf4e4647e7ef1158f05d8037a453cea904367
Jianzhi <ljzprivate@yahoo.com>
2019-06-21 16:34:41 -0700

    Updated generateTrajectoriesForNN.py, added preprocessTrajectoriesForNN.py

--- exec/generateTrajectoriesForNN.py
+++ exec/generateTrajectoriesForNN.py
@@ -1,18 +1,21 @@
 import sys
-sys.path.append("..")
+sys.path.append("../src/neuralNetwork")
+sys.path.append("../src/constrainedChasingEscapingEnv")
+sys.path.append("../src/algorithms")
+sys.path.append("../src")
 import os
 import numpy as np
 import pickle
 
-import src.constrainedChasingEscapingEnv.envNoPhysics as env
-import src.constrainedChasingEscapingEnv.policies as policies
-import src.constrainedChasingEscapingEnv.wrapperFunctions as wrapperFunctions
-import src.algorithms.mcts as mcts
-import src.play as play
-from exec.evaluationFunctions import GetSavePath
+import envNoPhysics as env
+import policies
+import wrapperFunctions
+import mcts
+import play
+from evaluationFunctions import GetSavePath
 
 
-def generateTrajectories():
+def main():
     # env
     wolfID = 0
     sheepID = 1
@@ -32,12 +35,10 @@ def generateTrajectories():
     transition = env.TransitionForMultiAgent(checkBoundaryAndAdjust)
     sheepTransition = lambda state, action: transition(np.array(state), [wolfDriectChasingPolicy(state), np.array(action)])
 
-    initPosition = np.array([[20, 20], [30, 30]])
+    initPosition = np.array([[30, 30], [20, 20]])
     initNoise = [0, 0]
     reset = env.Reset(numOfAgent, initPosition, initNoise)
     isTerminal = env.IsTerminal(getWolfPos, getSheepPos, killZoneRadius)
-
-    rewardFunction = lambda state, action: 1
 
     # mcts policy
     cInit = 1
@@ -52,6 +53,7 @@ def generateTrajectories():
 
     maxRollOutSteps = 10
     rolloutPolicy = lambda state: actionSpace[np.random.choice(range(numActionSpace))]
+    rewardFunction = lambda state, action: 1
     heuristic = lambda state: 0
     estimateValue = mcts.RollOut(rolloutPolicy, maxRollOutSteps, sheepTransition, rewardFunction, isTerminal, heuristic)
 
@@ -60,7 +62,7 @@ def generateTrajectories():
     mctsPolicyDistOutput = mcts.MCTS(numSimulations, selectChild, expand, estimateValue, mcts.backup, mcts.establishSoftmaxActionDist)
 
     # sample trajectories
-    maxRunningSteps = 100
+    maxRunningSteps = 30
 
     sampleTraj = play.SampleTrajectory(maxRunningSteps, transition, isTerminal, reset)
     policy = lambda state: [wolfDriectChasingPolicy(state), mctsPolicy(state)]
@@ -71,11 +73,12 @@ def generateTrajectories():
     policyDistOutput = lambda state: [wolfDriectChasingPolicy(state), mctsPolicyDistOutput(state)]
 
     useActionDist = True
-    numTrajs = 0
+    numTrajs = 100
     if not useActionDist:
         trajs = [sampleTraj(policy) for _ in range(numTrajs)]
     else:
         trajs = [sampleTrajWithActionDist(policyDistOutput) for _ in range(numTrajs)]
+    print("Avg traj length = {}".format(np.mean([len(traj) for traj in trajs])))
 
     dataDirectory = '../data/trainingDataForNN/trajectories'
     if not os.path.exists(dataDirectory):
@@ -99,4 +102,4 @@ def generateTrajectories():
 
 
 if __name__ == "__main__":
-    generateTrajectories()
+    main()
