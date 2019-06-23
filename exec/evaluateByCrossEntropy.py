245fa58dbd1b0517e652d7a4f98622820842db75
Enmin Zhou <zhouem14@gmail.com>
2019-06-23 15:50:57 -0700

    evaluateByCrossEntropy result

--- exec/evaluateByCrossEntropy.py
+++ exec/evaluateByCrossEntropy.py
@@ -15,28 +15,43 @@ import policies
 import mcts
 import os
 import math
+from collections import OrderedDict
+from evaluationFunctions import GetSavePath, ComputeStatistics, LoadTrajectories
+import pickle
 
 
-class Evaluate:
-    def __init__(self, mctsPolicy, nnPolicy, sampleFunction, colName, numOfAgnet, numPosEachState):
+class GenerateDistribution:
+    def __init__(self, mctsPolicy, nnPolicy, constructTestState, getSavePath, numTrials):
         self.mctsPolicy = mctsPolicy
         self.nnPolicy = nnPolicy
-        self.sampleFunction = sampleFunction
-        self.colName =colName
-        self.numPosEachState = numPosEachState
-        self.numOfAgent = numOfAgnet
+        self.constructTestState = constructTestState
+        self.numTrials = numTrials
+        self.getSavePath = getSavePath
 
     def __call__(self, df):
-        index = df.index[0]
-        state = [index[n:n+self.numPosEachState] for n in range(0, len(index), int(len(index)/self.numOfAgent))]
-        testState = self.sampleFunction(state)
+        wolfX = df.index.get_level_values('wolfXPosition')[0]
+        wolfY = df.index.get_level_values('wolfYPosition')[0]
+        sheepX = df.index.get_level_values('sheepXPosition')[0]
+        sheepY = df.index.get_level_values('sheepYPosition')[0]
+        indexLevelNames = df.index.names
+        parameters = {levelName: df.index.get_level_values(levelName)[0] for levelName in indexLevelNames}
+        saveFileName = self.getSavePath(parameters)
+        if os.path.exists(saveFileName):
+            print("Data exists {}".format(saveFileName))
+            return None
+        testState = self.constructTestState([wolfX, wolfY], [sheepX, sheepY])
         if len(testState) == 0:
-            return pd.Series({self.colName: 0})
-        mctsActionDistribution = [self.mctsPolicy(state) for state in testState]
-        nnActionDistribution = [self.nnPolicy([item for sublist in state for item in sublist]) for state in testState]
-        crossEntropyList = [calculateCrossEntropy(np.array(list(prediction.values())), np.array(list(target.values()))) for prediction, target in zip(nnActionDistribution, mctsActionDistribution)]
-        meanCrossEntropy = np.mean(crossEntropyList)
-        return pd.Series({self.colName: meanCrossEntropy})
+            zombieDistribution = [1 / 8 for cnt in range(8)]
+            data = [{"NNActionDistribution": zombieDistribution, 'mctsActionDistribution': zombieDistribution} for cnt in range(self.numTrials)]
+        else:
+            evaluateStates = [testState for cnt in range(self.numTrials)]
+            mctsActionDistribution = [list(self.mctsPolicy(state).values()) for state in evaluateStates]
+            nnActionDistribution = [list(self.nnPolicy(state).values()) for state in evaluateStates]
+            data = [{"NNActionDistribution": nnAD, 'mctsActionDistribution': mctsAD} for mctsAD, nnAD in zip(mctsActionDistribution, nnActionDistribution)]
+        file = open(saveFileName, 'wb')
+        pickle.dump(data, file)
+        file.close()
+        return data
 
 
 class DrawHeatMap:
@@ -56,24 +71,27 @@ class DrawHeatMap:
             plotDF.plot.scatter(x=self.subplotIndexName[0], y=self.subplotIndexName[1], c=colName, colormap="jet", ax=subplot, vmin=0, vmax=9)
             numOfplot = numOfplot + 1
         plt.subplots_adjust(wspace=0.8, hspace=0.4)
-        # plt.show()
 
 
-class SampleFunction:
-    def __init__(self, sampleNum, isTerminal):
-        self.sampleNum = sampleNum
+class ConstructTestState:
+    def __init__(self, sheepID, wolfID, isTerminal):
+        self.sheepID = sheepID
+        self.wolfID = wolfID
         self.isTerminal = isTerminal
 
-    def __call__(self, state):
-        return [state for count in range(self.sampleNum) if not self.isTerminal(state)]
+    def __call__(self, wolfPos, sheepPos):
+        unSequentialState = {self.wolfID:wolfPos, self.sheepID: sheepPos}
+        state = [unSequentialState[key] for key in sorted(unSequentialState.keys())]
+        if self.isTerminal(state):
+            return []
+        return state
 
 
 def main():
-    savePath = "../data/evaluateByCrossEntropy"
 
     # env
-    wolfID = 0
-    sheepID = 1
+    wolfID = 1
+    sheepID = 0
     posIndex = 0
     numOfAgent = 2
     numPosEachAgent = 2
@@ -96,7 +114,7 @@ def main():
     checkBoundaryAndAdjust = env.StayInBoundaryByReflectVelocity(xBoundary, yBoundary)
     wolfDriectChasingPolicy = policies.HeatSeekingDiscreteDeterministicPolicy(wolfActionSpace, getWolfPos, getSheepPos)
     transition = env.TransitionForMultiAgent(checkBoundaryAndAdjust)
-    sheepTransition = lambda state, action: transition(state, [wolfDriectChasingPolicy(state), np.array(action)])
+    sheepTransition = lambda state, action: transition(state, [np.array(action), wolfDriectChasingPolicy(state)])
     isTerminal = env.IsTerminal(getWolfPos, getSheepPos, killZoneRadius)
 
     rewardFunction = lambda state, action: 1
@@ -131,11 +149,9 @@ def main():
     # pandas
     wolfDiscreteFactor = 3
     sheepDiscreteFactor = 10
-    columnName = "cross_entropy"
     wolfDiscreteRange = xBoundary[1] / (wolfDiscreteFactor+1)
     sheepDiscreteRange = xBoundary[1] / (sheepDiscreteFactor+1)
-    samplePoints = 25
-    figureName = os.path.join(savePath, "Factor{}x{}_{}sample_rollout{}HeatMap.png".format(wolfDiscreteFactor, sheepDiscreteFactor, samplePoints, maxRollOutSteps))
+    numTrials = 25
     wolfXPosition = [wolfDiscreteRange * (i+1) for i in range(wolfDiscreteFactor)]
     wolfYPosition = wolfXPosition
     sheepXPosition = [sheepDiscreteRange * (i+1) for i in range(sheepDiscreteFactor)]
@@ -145,11 +161,28 @@ def main():
     diffIndex = pd.MultiIndex.from_product(levelValues, names=levelNames)
     toSplitFrame = pd.DataFrame(index=diffIndex)
 
-    sampleFunction = SampleFunction(samplePoints, isTerminal)
-    evaluate = Evaluate(mctsPolicy, nnPolicy, sampleFunction, columnName, numOfAgent, numPosEachAgent)
-    resultDF = toSplitFrame.groupby(levelNames).apply(evaluate)
+    constructTestState = ConstructTestState(sheepID=sheepID, wolfID=wolfID, isTerminal=isTerminal)
+    dataDirectory = "../data/evaluateByCrossEntropy/actionDistributions"
+    extension = ".pickle"
+    fixedParameters = OrderedDict()
+    fixedParameters['numSimulations'] = numSimulations
+    if not os.path.exists(dataDirectory):
+        os.makedirs(dataDirectory)
+    getSavePath = GetSavePath(dataDirectory, extension, fixedParameters)
+    generateDistribution = GenerateDistribution(mctsPolicy, nnPolicy, constructTestState, getSavePath, numTrials)
+    resultDF = toSplitFrame.groupby(levelNames).apply(generateDistribution)
+
+    loadData = LoadTrajectories(getSavePath)
+    computeStatistic = ComputeStatistics(loadData, numTrials, calculateCrossEntropy)
+    statisticDf = toSplitFrame.groupby(levelNames).apply(computeStatistic)
     drawHeatMap = DrawHeatMap(['wolfYPosition', 'wolfXPosition'], [len(wolfXPosition), len(wolfYPosition)], ['sheepXPosition', 'sheepYPosition'])
-    drawHeatMap(resultDF, columnName)
+    drawHeatMap(statisticDf, "mean")
+    figureSavePath = "../data/evaluateByCrossEntropy"
+    figureName = os.path.join(figureSavePath, "Factor{}x{}_{}sample_rollout{}HeatMap.png".format(wolfDiscreteFactor,
+                                                                                                 sheepDiscreteFactor,
+                                                                                                 numTrials,
+                                                                                                 maxRollOutSteps))
+
     plt.savefig(figureName)
 
 
