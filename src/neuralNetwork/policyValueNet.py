d6ff5f66dda969c0ce4114b7eb7d7e38f70b25d0
Enmin Zhou <zhouem14@gmail.com>
2019-06-24 17:49:34 -0700

    add learningRate modifier

--- src/neuralNetwork/policyValueNet.py
+++ src/neuralNetwork/policyValueNet.py
@@ -4,11 +4,10 @@ import random
 
 
 class GenerateModelSeparateLastLayer:
-	def __init__(self, numStateSpace, numActionSpace, learningRate, regularizationFactor,
+	def __init__(self, numStateSpace, numActionSpace, regularizationFactor,
 				 valueRelativeErrBound=0.01, seed=128):
 		self.numStateSpace = numStateSpace
 		self.numActionSpace = numActionSpace
-		self.learningRate = learningRate
 		self.regularizationFactor = regularizationFactor
 		self.valueRelativeErrBound = valueRelativeErrBound
 		self.seed = seed
@@ -24,6 +23,7 @@ class GenerateModelSeparateLastLayer:
 				state_ = tf.placeholder(tf.float32, [None, self.numStateSpace], name="state_")
 				actionLabel_ = tf.placeholder(tf.int32, [None, self.numActionSpace], name="actionLabel_")
 				valueLabel_ = tf.placeholder(tf.float32, [None, 1], name="valueLabel_")
+				updatedLearningRate_ = tf.placeholder(tf.float32, [None, 1], name="updatedLearningRate_")
 				actionLossCoef_ = tf.constant(50, dtype=tf.float32)
 				valueLossCoef_ = tf.constant(1, dtype=tf.float32)
 				tf.add_to_collection("inputs", state_)
@@ -31,6 +31,7 @@ class GenerateModelSeparateLastLayer:
 				tf.add_to_collection("inputs", valueLabel_)
 				tf.add_to_collection("lossCoefs", actionLossCoef_)
 				tf.add_to_collection("lossCoefs", valueLossCoef_)
+				tf.add_to_collection("learningRate", updatedLearningRate_)
 
 			with tf.name_scope("hidden"):
 				initWeight = tf.random_uniform_initializer(-0.03, 0.03)
@@ -105,7 +106,7 @@ class GenerateModelSeparateLastLayer:
 				tf.summary.scalar("l2RegLoss", l2RegularizationLoss_)
 				lossSummary = tf.summary.scalar("loss", loss_)
 
-				optimizer = tf.train.AdamOptimizer(self.learningRate, name='adamOpt_')
+				optimizer = tf.train.AdamOptimizer(updatedLearningRate_, name='adamOpt_')
 				gradVarPairs_ = optimizer.compute_gradients(loss_)
 				trainOp = optimizer.apply_gradients(gradVarPairs_)
 				tf.add_to_collection(tf.GraphKeys.TRAIN_OP, trainOp)
@@ -138,9 +139,10 @@ class GenerateModelSeparateLastLayer:
 
 
 class Train:
-	def __init__(self, maxStepNum, batchSize, terimnalController, coefficientController, trainReporter):
+	def __init__(self, maxStepNum, batchSize, lrModifier, terimnalController, coefficientController, trainReporter):
 		self.maxStepNum = maxStepNum
 		self.batchSize = batchSize
+		self.lrModifier = lrModifier
 		self.reporter = trainReporter
 		self.terminalController = terimnalController
 		self.coefficientController = coefficientController
@@ -149,6 +151,7 @@ class Train:
 		graph = model.graph
 		state_, actionLabel_, valueLabel_ = graph.get_collection_ref("inputs")
 		actionLossCoef_, valueLossCoef_ = graph.get_collection_ref("lossCoefs")
+		updatedLearningRate_ = graph.get_collection_ref("learningRate")
 		loss_ = graph.get_collection_ref("loss")[0]
 		actionLoss_ = graph.get_collection_ref("actionLoss")[0]
 		valueLoss_ = graph.get_collection_ref("valueLoss")[0]
@@ -168,8 +171,11 @@ class Train:
 			else:
 				stateBatch, actionLabelBatch, valueLabelBatch = sampleData(trainingDataList, self.batchSize)
 			actionLossCoef, valueLossCoef = self.coefficientController(evalDict)
-			feedDict = {state_: stateBatch, actionLabel_: actionLabelBatch, valueLabel_: valueLabelBatch, actionLossCoef_: actionLossCoef, valueLossCoef_: valueLossCoef}
+			updatedLearningRate = self.lrModifier(stepNum)
+			feedDict = {state_: stateBatch, actionLabel_: actionLabelBatch, valueLabel_: valueLabelBatch, actionLossCoef_: actionLossCoef, valueLossCoef_: valueLossCoef,
+			            updatedLearningRate_: updatedLearningRate}
 			evalDict, _, summary = model.run(fetches, feed_dict=feedDict)
+			evalDict.update({"learningRate": updatedLearningRate_})
 
 			self.reporter(evalDict, stepNum, trainWriter, summary)
 
