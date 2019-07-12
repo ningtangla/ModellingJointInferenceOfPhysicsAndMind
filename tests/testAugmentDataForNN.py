7b36e790526edfdea981a7b307e8c0cf79be3ad4
Enmin Zhou <zhouem14@gmail.com>
2019-07-12 04:11:50 -0700

    add augmentData tests

--- tests/testAugmentDataForNN.py
+++ tests/testAugmentDataForNN.py
@@ -10,7 +10,9 @@ import unittest
 from ddt import ddt, data, unpack
 import numpy as np
 import math
-from augmentDataForNN import GenerateSymmetricData, GetAgentStateFromDataSetState
+from augmentDataForNN import GenerateSymmetricData, \
+    GetAgentStateFromDataSetState, GenerateSymmetricState, \
+    GenerateSymmetricDistribution, CalibrateState
 from analyticGeometryFunctions import transitePolarToCartesian
 from dataTools import createSymmetricVector
 xBoundary = [0, 180]
@@ -23,7 +25,6 @@ ybias = yBoundary[1]
 class TestGenerateData(unittest.TestCase):
 
     def setUp(self):
-        self.bias = xBoundary[1]
         sheepSpeed = 20
         degrees = [
             math.pi / 2, 0, math.pi, -math.pi / 2, math.pi / 4,
@@ -41,13 +42,39 @@ class TestGenerateData(unittest.TestCase):
         ]
         agentStateDim = 2
         sheepID = 0
-        self.getSheepState = GetAgentStateFromDataSetState(agentStateDim, sheepID)
+        self.getSheepState = GetAgentStateFromDataSetState(
+            agentStateDim, sheepID)
         wolfID = 1
         self.getWolfState = GetAgentStateFromDataSetState(agentStateDim, wolfID)
 
+    @data((2, 2, [1, 0, 0, 1], [1, 1], [0, 1, 1, 0]))
+    @unpack
+    def testGenerateSymmetricState(self, numOfAgent, stateDim, state, symmetry,
+                                   groundTruth):
+        round = lambda state: np.round(state, 10)
+        calibrateState = CalibrateState(xBoundary, yBoundary, round)
+        generateSymmetricState = GenerateSymmetricState(numOfAgent, stateDim,
+                                                        createSymmetricVector,
+                                                        calibrateState)
+        testState = generateSymmetricState(state, np.array(symmetry))
+        self.assertTrue(np.allclose(testState, np.array(groundTruth)))
+
+    @data(([0.25, 0.3, 0, 0, 0.45, 0, 0,
+            0], [1, 1], [0.3, 0.25, 0, 0, 0.45, 0, 0, 0]))
+    @unpack
+    def testGenerateSymmetricDistribution(self, distribution, symmetry,
+                                          groundTruth):
+        generateSymmetricDistribution = GenerateSymmetricDistribution(
+            self.sheepActionSpace, createSymmetricVector)
+        symmetricDistribution = generateSymmetricDistribution(
+            distribution, np.array(symmetry))
+        self.assertTrue(np.allclose(symmetricDistribution, groundTruth))
+
     # (0, 1), (1, 0), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)
     @data((
-        [np.array([[10, 5, 20, 5], [0.25, 0.3, 0, 0, 0.45, 0, 0, 0]])],
+        [[10, 5, 20, 5], [0.25, 0.3, 0, 0, 0.45, 0, 0, 0]],
+        2,
+        2,
         [
             np.array([[10, 5, 20, 5], [0.25, 0.3, 0, 0, 0.45, 0, 0, 0]]),
             np.array([[5, 10, 5, 20], [0.3, 0.25, 0, 0, 0.45, 0, 0,
@@ -57,7 +84,8 @@ class TestGenerateData(unittest.TestCase):
             np.array([[10, -5 + ybias, 20, -5 + ybias],
                       [0, 0.3, 0, 0.25, 0, 0, 0.45, 0]]),  # symmetry: [1,0]
             np.array([[-5 + xbias, -10 + ybias, -5 + xbias, -20 + ybias],
-                      [0, 0, 0.25, 0.3, 0, 0.45, 0, 0]]),  # symmetry: [-1,1]
+                      [0, 0, 0.25, 0.3, 0, 0.45, 0, 0]]),
+            # symmetry: [-1,1]
             np.array([[-5 + xbias, 10, -5 + xbias, 20],
                       [0.3, 0, 0.25, 0, 0, 0, 0, 0.45]]),  # symmetry: [0,1]
             np.array([[5, -10 + ybias, 5, -20 + ybias],
@@ -67,18 +95,31 @@ class TestGenerateData(unittest.TestCase):
         ]  # symmetry: [-1,1]
     ))
     @unpack
-    def testGenerateSymmetricData(self, originalDataSet, groundTruth):
-        generateSymmetricData = GenerateSymmetricData(self.bias,
-                                                      createSymmetricVector,
-                                                      self.symmetries,
-                                                      self.sheepActionSpace,
-                                                      self.getSheepState,
-                                                      self.getWolfState)
-        symmetricDataSet = generateSymmetricData(originalDataSet)
-        symmetricDict = {tuple(np.round(data[0])): list(data[1]) for data in symmetricDataSet}
-        groundTruthDict = {tuple(data[0]): list(data[1]) for data in groundTruth}
+    def testGenerateSymmetricData(self, originalData, numOfAgent, stateDim,
+                                  groundTruth):
+        round = lambda state: np.round(state, 10)
+        calibrateState = CalibrateState(xBoundary, yBoundary, round)
+        generateSymmetricState = GenerateSymmetricState(numOfAgent, stateDim,
+                                                        createSymmetricVector,
+                                                        calibrateState)
+        generateSymmetricDistribution = GenerateSymmetricDistribution(
+            self.sheepActionSpace, createSymmetricVector)
+        generateSymmetricData = GenerateSymmetricData(
+            self.symmetries, createSymmetricVector, generateSymmetricState,
+            generateSymmetricDistribution)
+
+        symmetricDataSet = generateSymmetricData(originalData)
+        symmetricDict = {
+            tuple(np.round(data[0])): list(data[1]) for data in symmetricDataSet
+        }
+        groundTruthDict = {
+            tuple(data[0]): list(data[1]) for data in groundTruth
+        }
         for key in symmetricDict.keys():
-            self.assertTrue(np.all(np.array(symmetricDict[key]) == np.array(groundTruthDict[key])))
+            self.assertTrue(
+                np.all(
+                    np.array(symmetricDict[key]) == np.array(
+                        groundTruthDict[key])))
 
 
 if __name__ == "__main__":
