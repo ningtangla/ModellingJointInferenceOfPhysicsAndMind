8e30ce8e031a8f2d939777c6c6d14c9b0bfd164f
ningtang <ningtangzju@gmail.com>
2019-07-21 09:03:01 -0700

    add leased xml physical model and add its unittest for set q state and check x state

--- tests/testSetStateInLeasedModel.py
+++ tests/testSetStateInLeasedModel.py
@@ -19,9 +19,10 @@ class TestEnvMujoco(unittest.TestCase):
         self.nDimJointEachBody = 2
         self.nJoint = self.simulation.model.njnt
         self.nAgent = 3
-        self.agentJointIndex = list(range(self.nAgent))
-        self.ropeJointIndex = list(range(self.nAgent, int(self.nJoint/self.nDimJointEachBody)))
-        self.nRopeBody = int(len(self.ropeJointIndex) / self.nDimJointEachBody)
+        self.agentJointIndex = list(range(self.nAgent * self.nDimJointEachBody))
+        self.ropeJointIndex = list(range(self.nAgent * self.nDimJointEachBody, self.nJoint))
+        self.nRopeJoint = len(self.ropeJointIndex)
+        self.nRopeBody = int(self.nRopeJoint / self.nDimJointEachBody)
     
     def testXMLState(self):
         qPosFromXML = self.simulation.data.qpos
@@ -32,15 +33,37 @@ class TestEnvMujoco(unittest.TestCase):
     @data((np.array([-3, 3, 5, 5, -8, -8]), np.array([0, 0, 1, 1, -1, -1])))
     @unpack
     def testSetState(self, agentQPosToSet, action):
+        qPos = np.concatenate([agentQPosToSet, np.zeros(self.nRopeJoint)])
+        qPosFromXML = self.simulation.data.qpos
+        self.assertTrue(not np.all(qPos == qPosFromXML))
         
-        physicsSimulation.data.ctrl[:] = action
-        physicsViewer = mujoco.MjViewer(physicsSimulation)
-        physicsSimulation.step()
-        numSimulationFrames = 100000
+        self.simulation.data.qpos[:] = qPos
+        self.simulation.data.ctrl[:] = action
+        self.simulation.step()
+        self.simulation.forward()
+        newQPos = self.simulation.data.qpos[:]
+        self.assertTrue(not np.all(qPos == newQPos))
+        
+        self.simulation.data.qpos[:] = qPos
+        self.assertTrue(np.all(qPos == self.simulation.data.qpos[:]))
+        
+        physicsViewer = mujoco.MjViewer(self.simulation)
+        numSimulationFrames = 100
         for frameIndex in range(numSimulationFrames):
-            physicsSimulation.step()
-            physicsSimulation.forward()
+            self.simulation.data.qpos[:] = qPos
+            self.simulation.step()
+            self.simulation.forward()
             physicsViewer.render()
+
+    @data((np.array([-3, 3, 5, 5, -8, -8]), np.array([0, 0, 1, 1, -1, -1])))
+    @unpack
+    def testXPosEqualsQPos(self, agentQPosToSet, action):
+        qPos = np.concatenate([agentQPosToSet, np.zeros(self.nRopeJoint)])
+        self.simulation.data.qpos[:] = qPos
+        self.simulation.forward()
+        xPos2Dim = np.array([siteXPos[0:2] for siteXPos in self.simulation.data.site_xpos]).flatten()
+        self.assertTrue(np.all(qPos == xPos2Dim))
+
 
 if __name__ == '__main__':
     unittest.main()
