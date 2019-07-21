ed576a90d6580486e4376ebeec140a0869dfe651
ningtang <ningtangzju@gmail.com>
2019-07-21 06:09:10 -0700

    commit to checkout

--- tests/testSetStateInLeasedModel.py
+++ tests/testSetStateInLeasedModel.py
@@ -1,4 +1,5 @@
-
+import unittest
+from ddt import ddt, unpack, data
 import sys
 import os
 import mujoco_py as mujoco
@@ -7,31 +8,34 @@ import numpy as np
 DIRNAME = os.path.dirname(__file__)
 sys.path.append(os.path.join(DIRNAME, '..'))
 
-
+@ddt
 class TestEnvMujoco(unittest.TestCase):
-    def setUp():
+    def setUp(self):
         self.modelPath = os.path.join(DIRNAME, '..', 'env', 'xmls', 'leased.xml')
-        self.model = load_model_from_path(self.modelPath)
-        self.simulation = MjSim(self.model)
-        self.nDimQPos = len(self.simulation.data.qPos)
-        self.nDimQVel = len(self.simulation.data.qVel)
+        self.model = mujoco.load_model_from_path(self.modelPath)
+        self.simulation = mujoco.MjSim(self.model)
+        self.nDimQPos = len(self.simulation.data.qpos)
+        self.nDimQVel = len(self.simulation.data.qvel)
         self.nDimJointEachBody = 2
         self.nJoint = self.simulation.model.njnt
         self.nAgent = 3
         self.agentJointIndex = list(range(self.nAgent))
-        self.ropeJointIndex = list(range(self.nJoint/self.nDimJointEachBody)) - self.agentJointIndex
-        self.nRopeBody = len(self.ropeJointIndex) / self.nDimJoint
+        self.ropeJointIndex = list(range(self.nAgent, int(self.nJoint/self.nDimJointEachBody)))
+        self.nRopeBody = int(len(self.ropeJointIndex) / self.nDimJointEachBody)
     
-    @data((np.array([-3, 3, 5, 5, -8, -8]+[0]*int(self.nRopeBody)), np.array([0, 0, 1, 1, -1, -1]))
+    def testXMLState(self):
+        qPosFromXML = self.simulation.data.qpos
+        qVelFromXML = self.simulation.data.qvel
+        self.assertTrue(np.all(qPosFromXML == 0))
+        self.assertTrue(np.all(qVelFromXML == 0))
+   
+    @data((np.array([-3, 3, 5, 5, -8, -8]), np.array([0, 0, 1, 1, -1, -1])))
     @unpack
-    def testSetState(self, qPosToSet, action)
-        qPosFromXML = self.physicsSimulation.data.qPos
-        qVelFromXML = self.physicsSimulation.data.
-        self.assertTrue(np.all(
+    def testSetState(self, agentQPosToSet, action):
+        
         physicsSimulation.data.ctrl[:] = action
         physicsViewer = mujoco.MjViewer(physicsSimulation)
         physicsSimulation.step()
-        newQPos = 
         numSimulationFrames = 100000
         for frameIndex in range(numSimulationFrames):
             physicsSimulation.step()
@@ -39,4 +43,4 @@ class TestEnvMujoco(unittest.TestCase):
             physicsViewer.render()
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
