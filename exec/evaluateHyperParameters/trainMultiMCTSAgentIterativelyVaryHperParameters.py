493fa2c28b3ea4d9ae4062b9f6558833020804bc
v100 <coreknowledge666@gmail.com>
2019-07-31 02:36:54 +0800

    commit before toy run

--- exec/evaluateHyperParameters/trainMultiMCTSAgentIterativelyVaryHperParameters.py
+++ exec/evaluateHyperParameters/trainMultiMCTSAgentIterativelyVaryHperParameters.py
@@ -6,15 +6,20 @@ DIRNAME = os.path.dirname(__file__)
 sys.path.append(os.path.join(DIRNAME, '..', '..'))
 
 import numpy as np
-from collections import OrderedDict
-import pandas as pd
-
-from exec.parallelComputing import GenerateTrajectoriesParallel
-
-
 from subprocess import Popen, PIPE
 import json
 import math
+from collections import OrderedDict
+import pandas as pd
+from exec.parallelComputing import GenerateTrajectoriesParallel
+from src.constrainedChasingEscapingEnv.state import GetAgentPosFromState
+from matplotlib import pyplot as plt
+from src.constrainedChasingEscapingEnv.envMujoco import  IsTerminal
+from exec.trajectoriesSaveLoad import GetSavePath, LoadTrajectories, readParametersFromDf, loadFromPickle
+from exec.evaluationFunctions import ComputeStatistics
+from src.constrainedChasingEscapingEnv.state import GetAgentPosFromState
+from exec.preProcessing import AccumulateRewards
+from src.constrainedChasingEscapingEnv.reward import RewardFunctionCompete
 
 class TrainMultiMCTSAgentParallel:
     def __init__(self, codeFileName):
@@ -29,14 +34,18 @@ class TrainMultiMCTSAgentParallel:
         for proc in processList:
             proc.wait()
         return cmdList
+def drawPerformanceLine(dataDf, axForDraw, numSimulations, trainSteps):
+    for key, grp in dataDf.groupby('modelLearnRate'):
+        grp.index = grp.index.droplevel('modelLearnRate')
+        grp.plot(ax=axForDraw, title='numSimulations={}, trainSteps={}'.format(numSimulations, trainSteps), y='mean', yerr='std', marker='o', label='learnRate={}'.format(key))
 
 def main():
     # Mujoco environment
     print('start')
     manipulatedHyperVariables = OrderedDict()
-    manipulatedHyperVariables['miniBatchSize'] = [64, 256]  # [64, 128, 256]
-    manipulatedHyperVariables['learningRate'] = [1e-3, 1e-4, 1e-5]  # [1e-2, 1e-3, 1e-4]
-    manipulatedHyperVariables['numSimulations'] = [50] #[50, 100, 200]
+    manipulatedHyperVariables['miniBatchSize'] = [64, 32]  # [64, 128, 256]
+    manipulatedHyperVariables['learningRate'] = [1e-3, 1e-5]  # [1e-2, 1e-3, 1e-4]
+    manipulatedHyperVariables['numSimulations'] = [5,10] #[50, 100, 200]
 
     #numSimulations = manipulatedHyperVariables['numSimulations']
     levelNames = list(manipulatedHyperVariables.keys())
@@ -44,10 +53,10 @@ def main():
     modelIndex = pd.MultiIndex.from_product(levelValues, names=levelNames)
 
     hyperVariablesConditionlist=[]
-
-    for modelIndexNumber in range(len(modelIndex)):
-        oneCondition={levelName:str(modelIndex.get_level_values(levelName)[modelIndexNumber]) for levelName in levelNames}
-        hyperVariablesConditionlist.append(oneCondition)
+    hyperVariablesConditionlist=[{levelName:str(modelIndex.get_level_values(levelName)[modelIndexNumber]) for levelName in levelNames} for modelIndexNumber in range(len(modelIndex))]
+    # for modelIndexNumber in range(len(modelIndex)):
+    #     oneCondition={levelName:str(modelIndex.get_level_values(levelName)[modelIndexNumber]) for levelName in levelNames}
+    #     hyperVariablesConditionlist.append(oneCondition)
     print(hyperVariablesConditionlist)
     numTrajectoriesToStartTrain = 4 * 256
 
@@ -64,16 +73,82 @@ def main():
         preTrainCmdList = generateTrajectoriesParallel(trajectoryBeforeTrainPathParamters)
         print(preTrainCmdList)
 
-
     trainOneConditionFileName='trainMultiMCTSforOneCondition.py'
-
     trainMultiMCTSAgentParallel=TrainMultiMCTSAgentParallel(trainOneConditionFileName)
-
     trainCmdList=trainMultiMCTSAgentParallel(hyperVariablesConditionlist)
-
     print(trainCmdList)
 
 
+
+    # Evaluate Session
+    evluateVariables=manipulatedHyperVariables.copy()
+    evluateVariables['iterationIndex']=[0,5,10]
+    evluateLevelNames = list(evluateVariables.keys())
+    evluatelevelValues = list(evluateVariables.values())
+    evaluateModelIndex = pd.MultiIndex.from_product(evluatelevelValues, names=evluateLevelNames)
+    toSplitFrame = pd.DataFrame(index=evaluateModelIndex)
+    evaluatelevelNames=list(evluateVariables.keys())
+
+
+    #genarate trajectories
+    evalNumTrials = 10
+    generateTrajectoriesCodeName = 'generateTrajByNNWolfAndStationarySheepMujoco.py'
+    numCpuCores = os.cpu_count()
+    numCpuToUse = int(0.5*numCpuCores)
+    numToUseCores = min(evalNumTrials, numCpuToUse)
+    generateTrajectoriesParallel = GenerateTrajectoriesParallel(generateTrajectoriesCodeName, evalNumTrials,
+            numToUseCores)
+    generateTrajectoriesParallelFromDf = lambda df: generateTrajectoriesParallel(readParametersFromDf(df))
+    toSplitFrame.groupby(evaluatelevelNames).apply(generateTrajectoriesParallelFromDf)
+
+
+    #Measurements
+    sheepId = 0
+    wolfId = 1
+    xPosIndex = [2, 3]
+    getSheepXPos = GetAgentPosFromState(sheepId, xPosIndex)
+    getWolfXPos = GetAgentPosFromState(wolfId, xPosIndex)
+    killzoneRadius = 2
+    isTerminal = IsTerminal(killzoneRadius, getSheepXPos, getWolfXPos)
+    alivePenalty = -0.05
+    deathBonus = 1
+    rewardFunction = RewardFunctionCompete(alivePenalty, deathBonus, isTerminal)
+    decay = 1
+    accumulateRewards = AccumulateRewards(decay, rewardFunction)
+
+    trajectoryDirectory = os.path.join(DIRNAME, '..', '..', 'data', 'evaluateHyperParameters','evaluateNNPolicy', 'evaluationTrajectories')
+    trajectoryExtension = '.pickle'
+    trainMaxRunningSteps = 20
+    trajectoryFixedParameters = {'agentId': wolfId, 'maxRunningSteps': trainMaxRunningSteps,'killzoneRadius': killzoneRadius}
+
+    getTrajectorySavePath = GetSavePath(trajectoryDirectory, trajectoryExtension, trajectoryFixedParameters)
+    getTrajectorySavePathFromDf = lambda  df: getTrajectorySavePath(readParametersFromDf(df))
+
+    fuzzySearchParameterNames = ['sampleIndex']
+    loadTrajectories = LoadTrajectories(getTrajectorySavePath, loadFromPickle, fuzzySearchParameterNames)
+    loadTrajectoriesFromDf = lambda df: loadTrajectories(readParametersFromDf(df))
+    measurementFunction = lambda trajectory: accumulateRewards(trajectory)[0]
+    computeStatistics = ComputeStatistics(loadTrajectoriesFromDf, measurementFunction)
+    statisticsDf = toSplitFrame.groupby(evaluatelevelNames).apply(computeStatistics)
+
+    # plot the results
+    fig = plt.figure()
+    numColumns = len(evluateVariables['miniBatchSize'])
+    numRows = len(evluateVariables['numSimulations'])
+    plotCounter = 1
+
+    for  miniBatchSize, grp in statisticsDf.groupby('miniBatchSize'):
+        grp.index = grp.index.droplevel('miniBatchSize')
+        for numSimulation,subgrp in grp.groupby('numSimulations'):
+            subgrp.index = subgrp.index.droplevel('numSimulations')
+            axForDraw = fig.add_subplot(numRows, numColumns, plotCounter)
+            for lr,ssubgrp in subgrp.groupby('learningRate'):
+                ssubgrp.plot(y='mean', marker='o', label=lr, ax=axForDraw)
+            plotCounter += 1
+    plt.ylabel('Accumulated rewards')
+    plt.title('iterative training in chasing task with killzone radius = 2 \nStart with random model')
+    plt.legend(loc='best')
+    plt.show()
 
 if __name__ == '__main__':
     main()
