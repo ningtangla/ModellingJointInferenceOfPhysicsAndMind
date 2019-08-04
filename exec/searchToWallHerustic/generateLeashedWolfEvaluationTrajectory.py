2a22063c76f204b046cf283f3d50a0551459e28f
ningtang <ningtangzju@gmail.com>
2019-08-04 05:18:10 -0700

    finish frame of search

---
+++ exec/searchToWallHerustic/generateLeashedWolfEvaluationTrajectory.py
@@ -0,0 +1,174 @@
+import sys
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+DIRNAME = os.path.dirname(__file__)
+sys.path.append(os.path.join(DIRNAME, '..', '..'))
+
+import json
+from collections import OrderedDict
+import pickle
+import pandas as pd
+import time
+import mujoco_py as mujoco
+from matplotlib import pyplot as plt
+import numpy as np
+
+from src.constrainedChasingEscapingEnv.envMujoco import ResetUniformForLeashed, IsTerminal, TransitionFunction
+from src.algorithms.mcts import ScoreChild, SelectChild, InitializeChildren, Expand, MCTS, backup, \
+    establishPlainActionDist, RollOut
+from src.episode import SampleTrajectory, chooseGreedyAction
+from src.constrainedChasingEscapingEnv.policies import stationaryAgentPolicy, HeatSeekingDiscreteDeterministicPolicy, \
+    HeatSeekingContinuesDeterministicPolicy
+from exec.trajectoriesSaveLoad import GetSavePath, LoadTrajectories, readParametersFromDf, loadFromPickle, GenerateAllSampleIndexSavePaths, SaveAllTrajectories, saveToPickle
+from exec.evaluationFunctions import ComputeStatistics, GenerateInitQPosUniform
+from src.neuralNetwork.policyValueNet import GenerateModel, restoreVariables, ApproximatePolicy
+from src.constrainedChasingEscapingEnv.measure import DistanceBetweenActualAndOptimalNextPosition, \
+    ComputeOptimalNextPos, GetAgentPosFromTrajectory, GetStateFromTrajectory
+from src.constrainedChasingEscapingEnv.state import GetAgentPosFromState
+from src.constrainedChasingEscapingEnv.analyticGeometryFunctions import computeAngleBetweenVectors
+from exec.trainMCTSNNIteratively.valueFromNode import EstimateValueFromNode
+from src.constrainedChasingEscapingEnv.reward import RewardFunctionCompete, HeuristicDistanceToTarget, HeuristicDistanceToOtherAgentAndWall
+from exec.preProcessing import AccumulateRewards
+
+
+def main():
+    dirName = os.path.dirname(__file__)
+    trajectoryDirectory = os.path.join(DIRNAME, '..', '..', 'data', 'evaluateSupervisedLearning',
+                                       'evaluateLeashedTrajectories','mctsSheep')
+    if not os.path.exists(trajectoryDirectory):
+        os.makedirs(trajectoryDirectory)
+
+    trajectoryExtension = '.pickle'
+    trainMaxRunningSteps = 25
+    trainNumSimulations = 200
+    wolfId = 1
+    trajectoryFixedParameters = {'agentId': wolfId, 'maxRunningSteps': trainMaxRunningSteps, 'numSimulations': trainNumSimulations}
+
+    getTrajectorySavePath = GetSavePath(trajectoryDirectory, trajectoryExtension, trajectoryFixedParameters)
+
+    parametersForTrajectoryPath = json.loads(sys.argv[1])
+    startSampleIndex = int(sys.argv[2])
+    endSampleIndex = int(sys.argv[3])
+    parametersForTrajectoryPath['sampleIndex'] = (startSampleIndex, endSampleIndex)
+    trajectorySavePath = getTrajectorySavePath(parametersForTrajectoryPath)
+
+    if not os.path.isfile(trajectorySavePath):
+
+        # Mujoco environment
+        physicsDynamicsPath = os.path.join(dirName, '..', '..', 'env', 'xmls', 'leased.xml')
+        physicsModel = mujoco.load_model_from_path(physicsDynamicsPath)
+        physicsSimulation = mujoco.MjSim(physicsModel)
+        numAgents = 2
+        agentIds = list(range(numAgents))
+
+        sheepId = 0
+        wolfId = 1
+        xPosIndex = [2, 3]
+        getSheepXPos = GetAgentPosFromState(sheepId, xPosIndex)
+        getWolfXPos = GetAgentPosFromState(wolfId, xPosIndex)
+
+        killzoneRadius = 1
+        isTerminal = IsTerminal(killzoneRadius, getSheepXPos, getWolfXPos)
+
+        numSimulationFrames = 20
+        transit = TransitionFunction(physicsSimulation, isTerminal, numSimulationFrames)
+
+        originActionSpace = [(10, 0), (7, 7), (0, 10), (-7, 7), (-10, 0), (-7, -7), (0, -10), (7, -7)]
+        preyPowerRatio = float(parametersForTrajectoryPath['prayPowerRatio']
+        sheepActionSpace = list(map(tuple, np.array(originActionSpace) * preyPowerRatio))
+        predatorPowerRatio = 1.3
+        actionSpace = list(map(tuple, np.array(originActionSpace) * predatorPowerRatio))
+
+        numActionSpace = len(actionSpace)
+
+        alivePenalty = -0.05
+        deathBonus = 1
+        rewardFunction = RewardFunctionCompete(alivePenalty, deathBonus, isTerminal)
+
+        # neural network init and save path
+        numStateSpace = 18
+        numSheepStateSpace = 12
+        regularizationFactor = 1e-4
+        sharedWidths = [128]
+        actionLayerWidths = [128]
+        valueLayerWidths = [128]
+        generateModel = GenerateModel(numStateSpace, numActionSpace, regularizationFactor)
+        generateSheepModel = GenerateModel(numSheepStateSpace, numActionSpace, regularizationFactor)
+
+
+        NNFixedParameters = {'agentId': wolfId, 'maxRunningSteps': trainMaxRunningSteps, 'numSimulations': trainNumSimulations}
+
+        dirName = os.path.dirname(__file__)
+        NNModelSaveDirectory = os.path.join(DIRNAME, '..', '..', 'data', 'evaluateSupervisedLearning', 'trainedModels')
+        NNModelSaveExtension = ''
+        getNNModelSavePath = GetSavePath(NNModelSaveDirectory, NNModelSaveExtension, NNFixedParameters)
+
+        initNNModel = generateModel(sharedWidths * 4, actionLayerWidths, valueLayerWidths)
+        initSheepNNModel = generateSheepModel(sharedWidths * 4, actionLayerWidths, valueLayerWidths)
+
+        # generate a set of starting conditions to maintain consistency across all the conditions
+        # sample trajectory
+        qPosInit = (0, ) * 24
+        qVelInit = (0, ) * 24
+        qPosInitNoise = 7
+        qVelInitNoise = 5
+        numAgent = 3
+        tiedAgentId = [1, 2]
+        ropeParaIndex = list(range(3, 12))
+        maxRopePartLength = 0.25
+
+        evalNumTrials = 3
+        getResetFromTrial = lambda trial: ResetUniformForLeashed(physicsSimulation, qPosInit, qVelInit, numAgent, tiedAgentId, \
+                ropeParaIndex, maxRopePartLength, qPosInitNoise, qVelInitNoise)
+        evalMaxRunningSteps = 25
+        getSampleTrajectory = lambda trial: SampleTrajectory(evalMaxRunningSteps, transit, isTerminal, getResetFromTrial(trial), chooseGreedyAction)
+        allSampleTrajectories = [getSampleTrajectory(trial) for trial in range(evalNumTrials)]
+
+        # save evaluation trajectories
+        modelPath = os.path.join('..', '..', 'data', 'evaluate')
+        restoredModel = restoreVariables(initNNModel, modelPath)
+        wolfPolicy = ApproximatePolicy(restoredModel, actionSpace)
+
+
+        transitInSheepMCTSSimulation = \
+                lambda state, sheepSelfAction: transit(state, [sheepSelfAction, chooseGreedyAction(wolfPolicy(state[0:3])), \
+                chooseGreedyAction(stationaryAgentPolicy(state))])
+        # MCTS
+        cInit = 1
+        cBase = 100
+        calculateScore = ScoreChild(cInit, cBase)
+        selectChild = SelectChild(calculateScore)
+
+        getUniformActionPrior = lambda state: {action: 1/numActionSpace for action in actionSpace}
+        initializeChildrenUniformPrior = InitializeChildren(actionSpace, transitInSheepMCTSSimulation,
+                                                            getUniformActionPrior)
+        expand = Expand(isTerminal, initializeChildrenUniformPrior)
+
+        aliveBonus = 0.05
+        deathPenalty = -1
+        rewardFunction = RewardFunctionCompete(aliveBonus, deathPenalty, isTerminal)
+
+        rolloutPolicy = lambda state: actionSpace[np.random.choice(range(numActionSpace))]
+        rolloutHeuristicWeightToOtherAgent = 0.1
+        rolloutHeuristicWeightToWall = float(parametersForTrajectoryPath['heuristicWeightWallDis'])
+        maxRolloutSteps = 7
+        wallDisToCenter = 10
+        rolloutHeuristic = HeuristicDistanceToOtherAgentAndWall(rolloutHeuristicWeightToOtherAgent, rolloutHeuristicWeightToWall, wallDisToCenter, getWolfXPos, getSheepXPos)
+        rollout = RollOut(rolloutPolicy, maxRolloutSteps, transitInSheepMCTSSimulation, rewardFunction, isTerminal,
+                          rolloutHeuristic)
+
+        numSimulations = 200
+        mcts = MCTS(numSimulations, selectChild, expand, rollout, backup, establishPlainActionDist)
+
+
+#policy
+        policy = lambda state: [mcts(state), wolfPolicy(state[:3]), stationaryAgentPolicy(state)]
+
+        beginTime = time.time()
+        trajectories = [sampleTrajectory(policy) for sampleTrajectory in allSampleTrajectories[startSampleIndex:endSampleIndex]]
+        processTime = time.time() - beginTime
+        saveToPickle(trajectories, trajectorySavePath)
+        restoredModel.close()
+
+if __name__ == '__main__':
+    main()
--- src/constrainedChasingEscapingEnv/reward.py
+++ src/constrainedChasingEscapingEnv/reward.py
@@ -29,3 +29,21 @@ class HeuristicDistanceToTarget:
         reward = -self.weight * distance
 
         return reward
+
+class HeuristicDistanceToOtherAgentAndWall:
+    def __init__(self, weightOtherAgentDis, weightWallDis, wallDisToCenter, getPredatorPosition, getPreyPosition):
+        self.weightOtherAgentDis = weightOtherAgentDis
+        self.weighiWallDis = weightWallDis
+        self.wallDisToCenter = wallDisToCenter
+        self.getPredatorPosition = getPredatorPosition
+        self.getPreyPosition = getPreyPosition
+
+    def __call__(self, state):
+        predatorPos = self.getPredatorPosition(state)
+        preyPos = self.getPreyPosition(state)
+
+        toOtherAgentDistance = np.linalg.norm(predatorPos - preyPos, ord = 2)
+        minDisToWall = np.min(np.array([np.abs(preyPos - self.wallDisToCenter), np.abs(preyPos + self.wallDisToCenter)]).flatten()) 
+        reward = - (self.weightOtherAgentDis * distance + self.weightWallDis * 1/np.power(minDisToWall, 2))
+
+        return reward
