e53ab94afe61286f7b10750b37d404ba5431d1db
shaozhe <chengshaozhe@gmail.com>
2019-08-11 20:04:42 +0800

    exp demp

---
+++ exec/generateExpDemo/generateDemo.py
@@ -0,0 +1,162 @@
+from pygame.color import THECOLORS
+import os
+import pandas as pd
+import sys
+import numpy as np
+import itertools as it
+from collections import OrderedDict
+DIRNAME = os.path.dirname(__file__)
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from exec.trajectoriesSaveLoad import ConvertTrajectoryToStateDf, \
+    loadFromPickle, saveToPickle, LoadTrajectories, GetSavePath,conditionDfFromParametersDict
+from exec.generateExpDemo.trajectory import ScaleTrajectory, AdjustDfFPStoTraj
+from exec.generateExpDemo.chasingVisualization import InitializeScreen, DrawBackground, DrawState, ChaseTrialWithTraj,DrawStateWithRope, ChaseTrialWithRopeTraj
+
+def getFileName(parameters,fixedParameters):
+    allParameters = dict(list(parameters.items()) + list(fixedParameters.items()))
+    sortedParameters = sorted(allParameters.items())
+    nameValueStringPairs = [parameter[0] + '=' + str(parameter[1]) for parameter in sortedParameters]
+    fileName = '_'.join(nameValueStringPairs)
+
+    return fileName
+
+class GetAgentCoordinateFromTrajectoryAndStateDf:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+
+    def __call__(self, trajectory, df):
+        timeStep = df.index.get_level_values('timeStep')[0]
+        objectId = df.index.get_level_values('agentId')[0]
+        coordinates = trajectory[timeStep][objectId][self.coordinates]
+
+        return coordinates
+
+def main():
+    manipulatedVariables = OrderedDict()
+
+    # manipulatedVariables['draggerMass'] = [8, 10, 12]
+    # manipulatedVariables['maxTendonLength'] = [0.6]
+    # manipulatedVariables['predatorMass'] = [10]
+    # manipulatedVariables['predatorPower'] = [1, 1.3, 1.6]
+    # manipulatedVariables['tendonDamping'] =[0.7]
+    # manipulatedVariables['tendonStiffness'] = [10]
+
+    manipulatedVariables['agentId'] = [0]
+    manipulatedVariables['maxRunningSteps'] = [100]
+    manipulatedVariables['numSimulations'] = [200]
+    manipulatedVariables['killzoneRadius'] = [2]
+    manipulatedVariables['offset'] = [0]
+
+    # manipulatedVariables['sampleIndex'] = [(0,1)]
+    # manipulatedVariables['miniBatchSize'] = [256]#[64, 128, 256, 512]
+    # manipulatedVariables['learningRate'] =  [1e-4]#[1e-2, 1e-3, 1e-4, 1e-5]
+    # manipulatedVariables['depth'] = [4]#[2,4, 6, 8]
+    # manipulatedVariables['trainSteps'] = [20000]#list(range(0,100001, 20000))
+
+    # manipulatedVariables['safeBound'] = [1.5]
+    # manipulatedVariables['preyPowerRatio'] =[0.7]
+    # manipulatedVariables['wallPunishRatio'] = [0.6]
+
+    productedValues = it.product(*[[(key, value) for value in values] for key, values in manipulatedVariables.items()])
+    conditionParametersAll = [dict(list(i)) for i in productedValues]
+
+    trajectoryFixedParameters = {}
+    trajectoryDirectory = os.path.join(DIRNAME, '..', '..', 'data', 'generateExpDemo','trajectories')
+
+    trajectoryExtension = '.pickle'
+    getTrajectorySavePath = GetSavePath(trajectoryDirectory, trajectoryExtension, trajectoryFixedParameters)
+    # fuzzySearchParameterNames = ['sampleIndex']
+    fuzzySearchParameterNames = []
+    loadTrajectories = LoadTrajectories(getTrajectorySavePath, loadFromPickle,fuzzySearchParameterNames)
+
+    trajectory = loadTrajectories(conditionParametersAll[0])
+
+    getRangeNumAgentsFromTrajectory = lambda trajectory: list(range(np.shape(trajectory[0])[0]))
+    getRangeTrajectoryLength = lambda trajectory: list(range(len(trajectory)))
+    getAllLevelValuesRange = {'timeStep': getRangeTrajectoryLength, 'agentId': getRangeNumAgentsFromTrajectory}
+
+    getAgentPosXCoord = GetAgentCoordinateFromTrajectoryAndStateDf(0)
+    getAgentPosYCoord = GetAgentCoordinateFromTrajectoryAndStateDf(1)
+    extractColumnValues = {'xPos': getAgentPosXCoord, 'yPos': getAgentPosYCoord}
+
+    convertTrajectoryToStateDf = ConvertTrajectoryToStateDf(getAllLevelValuesRange, conditionDfFromParametersDict,extractColumnValues)
+
+
+#### convert traj pickle to df
+    for conditionParameters in conditionParametersAll:
+        trajectories = loadTrajectories(conditionParameters)
+        numTrajectories = len(trajectories)
+        maxNumTrajectories = 10
+        numTrajectoryChoose = min(numTrajectories, maxNumTrajectories)
+        selectedTrajectories = trajectories[0:numTrajectoryChoose]
+
+        selectedDf = [convertTrajectoryToStateDf(trajectory) for trajectory in selectedTrajectories]
+
+        dataFileName = getFileName(conditionParameters,trajectoryFixedParameters)
+        imageSavePath = os.path.join(trajectoryDirectory, dataFileName)
+        if not os.path.exists(imageSavePath):
+            os.makedirs(imageSavePath)
+
+        [saveToPickle(df, os.path.join(imageSavePath, 'sampleIndex={}.pickle'.format(sampleIndex))) for df, sampleIndex in zip(selectedDf, range(numTrajectories))]
+
+### generate demo image
+        screenWidth = 800
+        screenHeight = 800
+        fullScreen = False
+        initializeScreen = InitializeScreen(screenWidth, screenHeight, fullScreen)
+        screen = initializeScreen()
+        leaveEdgeSpace = 200
+        lineWidth = 3
+        xBoundary = [leaveEdgeSpace, screenWidth - leaveEdgeSpace * 2]
+        yBoundary = [leaveEdgeSpace, screenHeight - leaveEdgeSpace * 2]
+        screenColor = THECOLORS['black']
+        lineColor = THECOLORS['white']
+        ropeColor = THECOLORS['white']
+        drawBackground = DrawBackground(screen, screenColor, xBoundary, yBoundary, lineColor, lineWidth)
+        circleSize = 10
+        positionIndex = [0, 1]
+
+
+
+        numOfAgent = 4
+        sheepId = 0
+        wolfId = 1
+        masterId = 2
+        distractorId = 3
+
+        conditionList = [[wolfId, masterId]]
+        # conditionList = [[wolfId, masterId],[wolfId, distractorId], None]
+
+        drawState = DrawState(screen, circleSize, numOfAgent, positionIndex, drawBackground)
+        drawStateWithRope = DrawStateWithRope(screen, circleSize, numOfAgent, positionIndex, ropeColor, drawBackground)
+
+        colorSpace = [THECOLORS['green'], THECOLORS['red'], THECOLORS['blue'], THECOLORS['yellow']]
+        circleColorList = colorSpace[:numOfAgent]
+
+        for index in range(numTrajectoryChoose):
+            for condition in conditionList:
+                imageFolderName = os.path.join("{}".format(index), 'condition='"{}".format(condition))
+                saveImageDir = os.path.join(os.path.join(imageSavePath, imageFolderName))
+
+                FPS = 60
+                chaseTrial = ChaseTrialWithTraj(FPS, circleColorList, drawState, saveImage=True, saveImageDir=saveImageDir)
+                chaseTrialWithRope = ChaseTrialWithRopeTraj(FPS, circleColorList, drawStateWithRope, saveImage=True, saveImageDir=saveImageDir)
+
+                rawXRange = [-10, 10]
+                rawYRange = [-10, 10]
+                scaledXRange = [200, 600]
+                scaledYRange = [200, 600]
+                scaleTrajectory = ScaleTrajectory(positionIndex, rawXRange, rawYRange, scaledXRange, scaledYRange)
+
+                oldFPS = 5
+                adjustFPS = AdjustDfFPStoTraj(oldFPS, FPS)
+
+                getTrajectory = lambda trajectoryDf: scaleTrajectory(adjustFPS(trajectoryDf))
+                trajectoryDf = pd.read_pickle(os.path.join(imageSavePath, 'sampleIndex={}.pickle'.format(index)))
+                trajectory = getTrajectory(trajectoryDf)
+                # chaseTrial(trajectory)
+                chaseTrialWithRope(trajectory, condition)
+
+if __name__ == '__main__':
+    main()
---
+++ exec/generateExpDemo/script.sh
@@ -0,0 +1,65 @@
+cd ~/ModellingJointInferenceOfPhysicsAndMind/data/searchToWallHerustic/mctsSheep/
+mkdir demo
+
+for heuristicWeightWallDis in 0.5 1 2
+do
+    for preyPowerRatio in 0.4 0.6 0.8
+    do
+        for index in 1 2
+        do
+        cd ~/ModellingJointInferenceOfPhysicsAndMind/data/searchToWallHerustic/mctsSheep/heuristicWeightWallDis=${heuristicWeightWallDis}_preyPowerRatio=${preyPowerRatio}
+        cd ${index}
+        ffmpeg -r 60 -f image2 -s 1920x1080 -i %04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ~/ModellingJointInferenceOfPhysicsAndMind/data/searchToWallHerustic/mctsSheep/demo/heuristicWeightWallDis=${heuristicWeightWallDis}_preyPowerRatio=${preyPowerRatio}_Demo${index}.mp4
+        done
+    done
+done
+cd ~/ModellingJointInferenceOfPhysicsAndMind/exec/generateVideosForLeashedDemo
+
+
+
+# cd ~/ModellingJointInferenceOfPhysicsAndMind/data/agentId=0_depth=8_learningRate=0.0001_maxRunningSteps=100_miniBatchSize=128_numSimulations=100_trainSteps=350000/
+# mkdir demo
+
+# for index in 0 1 2 3 4 5 6 7 8 9
+# do
+# cd ~/ModellingJointInferenceOfPhysicsAndMind/data/agentId=0_depth=8_learningRate=0.0001_maxRunningSteps=100_miniBatchSize=128_numSimulations=100_trainSteps=350000/${index}
+
+# ffmpeg -r 60 -f image2 -s 1920x1080 -i %04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ~/ModellingJointInferenceOfPhysicsAndMind/data/agentId=0_depth=8_learningRate=0.0001_maxRunningSteps=100_miniBatchSize=128_numSimulations=100_trainSteps=350000/demo/agentId=0_depth=8_learningRate=0.0001_maxRunningSteps=100_miniBatchSize=128_numSimulations=100_trainSteps=350000_Demo${index}.mp4
+# # mv *.Demo${index}.mp4 ../../demo
+# done
+
+# cd ~/ModellingJointInferenceOfPhysicsAndMind/exec/generateVideosForLeashedDemo
+
+
+
+
+# cd ~/ModellingJointInferenceOfPhysicsAndMind/data/searchLeashedModelParameters/leasedTrajectories/
+# mkdir demo
+
+# for draggerMass in 8 10 12
+# do
+#     for maxTendonLength in  0.6
+#     do
+#         for predatorMass in 10
+#         do
+#             for tendonDamping in 0.7
+#             do
+#                 for tendonStiffness in 10
+#                 do
+#                     for index in 0 1 2
+#                     do
+#                         mkdir ~/ModellingJointInferenceOfPhysicsAndMind/data/searchLeashedModelParameters/leasedTrajectories/draggerMass=${draggerMass}_maxTendonLength=${maxTendonLength}_predatorMass=${predatorMass}_tendonDamping=${tendonDamping}_tendonStiffness=${tendonStiffness}/demo
+
+
+#                             cd ~/ModellingJointInferenceOfPhysicsAndMind/data/searchLeashedModelParameters/leasedTrajectories/draggerMass=${draggerMass}_maxTendonLength=${maxTendonLength}_predatorMass=${predatorMass}_predatorPower=${predatorPower}_tendonDamping=${tendonDamping}_tendonStiffness=${tendonStiffness}/${index}
+
+#                             ffmpeg -r  60 -f image2 -s 1920x1080 -i  %04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ~/ModellingJointInferenceOfPhysicsAndMind/data/searchLeashedModelParameters/leasedTrajectories/demo/draggerMass=${draggerMass}_maxTendonLength=${maxTendonLength}_predatorMass=${predatorMass}_predatorPower=${predatorPower}_tendonDamping=${tendonDamping}_tendonStiffness=${tendonStiffness}_sampleIndex${index}.mp4
+#                         done
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
+
+# cd ~/ModellingJointInferenceOfPhysicsAndMind/exec/generateVideosForLeashedDemo
