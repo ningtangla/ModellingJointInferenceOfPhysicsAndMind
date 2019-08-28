9b6a25558671f733003e65c6f162066664989406
shaozhe <godlike0716@gmail.com>
2019-08-28 15:39:29 +0800

    multi agent resnet

--- exec/parallelComputing.py
+++ exec/parallelComputing.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 import itertools as it
 
+
 class GenerateTrajectoriesParallel:
     def __init__(self, codeFileName, numSample, numCmdList):
         self.codeFileName = codeFileName
@@ -11,18 +12,18 @@ class GenerateTrajectoriesParallel:
         self.numCmdList = numCmdList
 
     def __call__(self, parameters):
-        startSampleIndexes = np.arange(0, self.numSample, math.ceil(self.numSample/self.numCmdList))
+        startSampleIndexes = np.arange(0, self.numSample, math.ceil(self.numSample / self.numCmdList))
         endSampleIndexes = np.concatenate([startSampleIndexes[1:], [self.numSample]])
         startEndIndexesPair = zip(startSampleIndexes, endSampleIndexes)
         parametersString = dict([(key, str(value)) for key, value in parameters.items()])
         parametersStringJS = json.dumps(parametersString)
-        cmdList = [['python3', self.codeFileName, parametersStringJS, str(startSampleIndex), str(endSampleIndex)] 
-                for startSampleIndex, endSampleIndex in startEndIndexesPair]
+        cmdList = [['python3', self.codeFileName, parametersStringJS, str(startSampleIndex), str(endSampleIndex)] for startSampleIndex, endSampleIndex in startEndIndexesPair]
         print(cmdList)
         processList = [Popen(cmd, stdout=PIPE, stderr=PIPE) for cmd in cmdList]
         for proc in processList:
             proc.wait()
         return cmdList
+
 
 class ExcuteCodeOnConditionsParallel:
     def __init__(self, codeFileName, numSample, numCmdList):
@@ -32,13 +33,13 @@ class ExcuteCodeOnConditionsParallel:
 
     def __call__(self, conditions):
         assert self.numCmdList >= len(conditions), "condition number > cmd number, use more cores or less conditions"
-        numCmdListPerCondition = math.floor(self.numCmdList/len(conditions))
-        startSampleIndexes = np.arange(0, self.numSample, math.ceil(self.numSample/numCmdListPerCondition))
+        numCmdListPerCondition = math.floor(self.numCmdList / len(conditions))
+        startSampleIndexes = np.arange(0, self.numSample, math.ceil(self.numSample / numCmdListPerCondition))
         endSampleIndexes = np.concatenate([startSampleIndexes[1:], [self.numSample]])
         startEndIndexesPair = zip(startSampleIndexes, endSampleIndexes)
         conditionStartEndIndexesPair = list(it.product(conditions, startEndIndexesPair))
         cmdList = [['python3', self.codeFileName, json.dumps(condition), str(startEndSampleIndex[0]), str(startEndSampleIndex[1])]
-                for condition, startEndSampleIndex in conditionStartEndIndexesPair]
+                   for condition, startEndSampleIndex in conditionStartEndIndexesPair]
         print(cmdList)
         processList = [Popen(cmd, stdout=PIPE, stderr=PIPE) for cmd in cmdList]
         for proc in processList:
