32e82df8af11658033e1af7d60ea8f5933725802
shaozhe <shaozhecheng@outlook.com>
2020-03-21 15:00:30 +0800

    rename SL

--- exec/multiChasingNoPhysics/iterativelyTrainCenterControlChasingNoPhysics/sampleMCTSSheepSingleChasingNoPhysics.py
+++ exec/multiChasingNoPhysics/iterativelyTrainCenterControlChasingNoPhysics/sampleMCTSSheepSingleChasingNoPhysics.py
@@ -19,134 +19,99 @@ from src.constrainedChasingEscapingEnv.envNoPhysics import  TransiteForNoPhysics
 
 import time
 from exec.trajectoriesSaveLoad import GetSavePath, saveToPickle
-<<<<<<< HEAD
 
 def main():
     parametersForTrajectoryPath = json.loads(sys.argv[1])
     startSampleIndex = int(sys.argv[2])
     endSampleIndex = int(sys.argv[3])
-    agentId = int(parametersForTrajectoryPath['agentId'])
-    parametersForTrajectoryPath['sampleIndex'] = (startSampleIndex, endSampleIndex)
-=======
->>>>>>> origin/multiChasingNoPhyscis
 
-def main():
-    parametersForTrajectoryPath = json.loads(sys.argv[1])
-    startSampleIndex = int(sys.argv[2])
-    endSampleIndex = int(sys.argv[3])
-    parametersForTrajectoryPath['sampleIndex'] = (startSampleIndex, endSampleIndex)
-
-<<<<<<< HEAD
-    ##test
-    # parametersForTrajectoryPath={}
-    # startSampleIndex=0
-    # endSampleIndex=1
-    ##test
-
-    killzoneRadius = 15
-    numSimulations = 100
-    maxRunningSteps = 150
-    fixedParameters = {'maxRunningSteps': maxRunningSteps, 'numSimulations': numSimulations, 'killzoneRadius': killzoneRadius}
-    trajectorySaveExtension = '.pickle'
-    dirName = os.path.dirname(__file__)
-    trajectoriesSaveDirectory = os.path.join(dirName, '..','..', '..', 'data', 'evaluateEscapeMultiChasingNoPhysics', 'trajectoriesWithWall')
-=======
     agentId = 0
-    
     # parametersForTrajectoryPath={}
     # startSampleIndex=0
     # endSampleIndex=100
+    # parametersForTrajectoryPath['sampleIndex'] = (startSampleIndex, endSampleIndex)
 
     killzoneRadius = 30
-    numSimulations = 200
-    maxRunningSteps = 150
+    # imageFolderName = 'preyPowerRatio='+ str(preyPowerRatio) + '_predatorPowerRatio=' + str(predatorPowerRatio) +'_killzoneRadius=' + str(killzoneRadius)
 
+    numSimulations = 50
+    maxRunningSteps = 150
     fixedParameters = {'maxRunningSteps': maxRunningSteps, 'numSimulations': numSimulations, 'killzoneRadius': killzoneRadius}
     trajectorySaveExtension = '.pickle'
     dirName = os.path.dirname(__file__)
-    trajectoriesSaveDirectory = os.path.join(dirName, '..','..', '..', 'data','evaluateEscapeMultiChasingNoPhysics', 'trajectoriesNoWallPunish')
->>>>>>> origin/multiChasingNoPhyscis
+    trajectoriesSaveDirectory = os.path.join(dirName, '..','..', '..', 'data','evaluateEscapeSingleChasingNoPhysics', 'trajectoriesWithStillAction')
+
     if not os.path.exists(trajectoriesSaveDirectory):
         os.makedirs(trajectoriesSaveDirectory)
     generateTrajectorySavePath = GetSavePath(trajectoriesSaveDirectory, trajectorySaveExtension, fixedParameters)
 
-
     trajectorySavePath = generateTrajectorySavePath(parametersForTrajectoryPath)
-<<<<<<< HEAD
 
     # while True:
-
-=======
->>>>>>> origin/multiChasingNoPhyscis
-    if not os.path.isfile(trajectorySavePath):
-        numOfAgent = 3
+    if  not os.path.isfile(trajectorySavePath):
+        numOfAgent = 2
         sheepId = 0
         wolfId = 1
-        wolf2Id = 2
+        # wolf2Id = 2
         positionIndex = [0, 1]
 
         xBoundary = [0,600]
         yBoundary = [0,600]
 
         #prepare render
-<<<<<<< HEAD
-        # from exec.evaluateNoPhysicsEnvWithRender import Render, SampleTrajectoryWithRender
-=======
-        # from exec.evaluateNoPhysicsEnvWithRender import Render #SampleTrajectoryWithRender
->>>>>>> origin/multiChasingNoPhyscis
-        # import pygame as pg
-        # renderOn = False
-        # from pygame.color import THECOLORS
-        # screenColor = THECOLORS['black']
-        # circleColorList = [THECOLORS['green'], THECOLORS['red'],THECOLORS['orange']]
-        # circleSize = 10
-        # screen = pg.display.set_mode([xBoundary[1], yBoundary[1]])
-        # saveImage = False
-        # saveImageDir = ''
-        # render = Render(numOfAgent, positionIndex,
-        #                 screen, screenColor, circleColorList, circleSize,saveImage,saveImageDir)
+        from exec.evaluateNoPhysicsEnvWithRender import Render #SampleTrajectoryWithRender
+
+        import pygame as pg
+        renderOn = True
+        from pygame.color import THECOLORS
+        screenColor = THECOLORS['black']
+        circleColorList = [THECOLORS['green'], THECOLORS['red'],THECOLORS['orange']]
+        circleSize = 10
+        screen = pg.display.set_mode([xBoundary[1], yBoundary[1]])
+
+        saveImage = True
+        saveImageDir = os.path.join(dirName, '..','..', '..', 'data','demoImg')
+        if not os.path.exists(saveImageDir):
+            os.makedirs(saveImageDir)
+        render = Render(numOfAgent, positionIndex,
+                        screen, screenColor, circleColorList, circleSize, saveImage, saveImageDir)
 
         getPreyPos = GetAgentPosFromState(sheepId, positionIndex)
         getPredatorPos = GetAgentPosFromState(wolfId, positionIndex)
-        getPredator2Pos=GetAgentPosFromState(wolf2Id, positionIndex)
+        # getPredator2Pos=GetAgentPosFromState(wolf2Id, positionIndex)
         stayInBoundaryByReflectVelocity = env.StayInBoundaryByReflectVelocity(xBoundary, yBoundary)
 
+
+        # isTerminal1 = env.IsTerminal(getPredatorPos, getPreyPos, killzoneRadius)
+        # isTerminal2 =env.IsTerminal(getPredator2Pos, getPreyPos, killzoneRadius)
+
+        # isTerminal=lambda state:isTerminal1(state) or isTerminal1(state)
         divideDegree=5
-
-        isTerminal1 = IsTerminal(getPredatorPos, getPreyPos, killzoneRadius,divideDegree)
-        isTerminal2 = IsTerminal(getPredator2Pos, getPreyPos, killzoneRadius,divideDegree)
-        isTerminal=lambda state,state2: isTerminal1(state,state2) or isTerminal2(state,state2)
-
-        # isTerminal = IsTerminal(getPredatorPos, getPreyPos, killzoneRadius,divideDegree)
+        isTerminal = IsTerminal(getPredatorPos, getPreyPos, killzoneRadius,divideDegree)
         transitionFunction = env.TransiteForNoPhysics(stayInBoundaryByReflectVelocity)
 
         reset = env.Reset(xBoundary, yBoundary, numOfAgent)
 
         actionSpace = [(10, 0), (7, 7), (0, 10), (-7, 7),
                        (-10, 0), (-7, -7), (0, -10), (7, -7)]
+        numActionSpace = len(actionSpace)
 
 
         preyPowerRatio = 3
         sheepActionSpace = list(map(tuple, np.array(actionSpace) * preyPowerRatio))
-        sheepActionSpace.append((0,0))
-<<<<<<< HEAD
+        # sheepActionSpace.append((0,0))
 
-=======
->>>>>>> origin/multiChasingNoPhyscis
         predatorPowerRatio = 2
+
         wolfActionSpace = list(map(tuple, np.array(actionSpace) * predatorPowerRatio))
 
-        numActionSpace = len(sheepActionSpace)
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/multiChasingNoPhyscis
 
         wolf1Policy = HeatSeekingDiscreteDeterministicPolicy(
             wolfActionSpace, getPredatorPos, getPreyPos, computeAngleBetweenVectors)
 
-        wolf2Policy=HeatSeekingDiscreteDeterministicPolicy(
-            wolfActionSpace, getPredator2Pos, getPreyPos, computeAngleBetweenVectors)
+        # wolf2Policy=HeatSeekingDiscreteDeterministicPolicy(
+            # wolfActionSpace, getPredator2Pos, getPreyPos, computeAngleBetweenVectors)
         # select child
         cInit = 1
         cBase = 100
@@ -157,10 +122,10 @@ def main():
         getActionPrior = lambda state: {action: 1 / len(sheepActionSpace) for action in sheepActionSpace}
 
     # load chase nn policy
-        # def sheepTransit(state, action): return transitionFunction(
-            # state, [action, chooseGreedyAction(wolf1Policy(state))])
         def sheepTransit(state, action): return transitionFunction(
-            state, [action, chooseGreedyAction(wolf1Policy(state)), chooseGreedyAction(wolf2Policy(state))])
+            state, [action, chooseGreedyAction(wolf1Policy(state))])
+        # def sheepTransit(state, action): return transitionFunction(
+        #     state, [action, chooseGreedyAction(wolf1Policy(state)), chooseGreedyAction(wolf2Policy(state))])
 
         # reward function
 
@@ -170,12 +135,13 @@ def main():
             aliveBonus, deathPenalty, isTerminal)
 
         # reward function with wall
-        safeBound = 80
-        wallDisToCenter = xBoundary[-1]/2
-        wallPunishRatio = 3
-        rewardFunction = reward.RewardFunctionWithWall(aliveBonus, deathPenalty, safeBound, wallDisToCenter, wallPunishRatio, isTerminal,getPreyPos)
+        # safeBound = 80
+        # wallDisToCenter = xBoundary[-1]/2
+        # wallPunishRatio = 3
+        # rewardFunction = reward.RewardFunctionWithWall(aliveBonus, deathPenalty, safeBound, wallDisToCenter, wallPunishRatio, isTerminal,getPreyPos)
 
         # initialize children; expand
+
 
         initializeChildren = InitializeChildren(
             sheepActionSpace, sheepTransit, getActionPrior)
@@ -194,29 +160,27 @@ def main():
         rollout = RollOut(rolloutPolicy, maxRolloutSteps, sheepTransit,
                           rewardFunction, isTerminal, rolloutHeuristic)
 
-        sheepPolicy = MCTS(numSimulations, selectChild, expand, rollout, backup, establishSoftmaxActionDist)
+        sheepPolicy = MCTS(numSimulations, selectChild, expand,
+                    rollout, backup, establishSoftmaxActionDist)
 
         # All agents' policies
 
-        # policy = lambda state:[sheepPolicy(state),wolf1Policy(state)]
-        policy = lambda state:[sheepPolicy(state),wolf1Policy(state),wolf2Policy(state)]
+        policy = lambda state:[sheepPolicy(state),wolf1Policy(state)]
+        # policy = lambda state:[sheepPolicy(state),wolf1Policy(state),wolf2Policy(state)]
 
 
-        sampleTrajectory=SampleTrajectory(maxRunningSteps, transitionFunction, isTerminal, reset, chooseGreedyAction)
+        # sampleTrajectory=SampleTrajectory(maxRunningSteps, transitionFunction, isTerminal, reset, chooseGreedyAction)
 
-        # sampleTrajectory = SampleTrajectoryWithRender(maxRunningSteps, transitionFunction, isTerminal, reset, chooseGreedyAction,render,renderOn)
+        sampleTrajectory = SampleTrajectoryWithRender(maxRunningSteps, transitionFunction, isTerminal, reset, chooseGreedyAction,render,renderOn)
 
         startTime = time.time()
         trajectories = [sampleTrajectory(policy) for sampleIndex in range(startSampleIndex, endSampleIndex)]
         saveToPickle(trajectories, trajectorySavePath)
         finshedTime = time.time() - startTime
-<<<<<<< HEAD
 
         print('lenght:',len(trajectories[0]))
-        print('timeTaken:',finshedTime)
 
-=======
-        print(finshedTime)
+        print('timeTaken:',finshedTime)
 
 class RollOut:
     def __init__(self, rolloutPolicy, maxRolloutStep, transitionFunction, rewardFunction, isTerminal, rolloutHeuristic):
@@ -258,8 +222,7 @@ class IsTerminal():
         self.getPreyPos = getPreyPos
         self.minDistance = minDistance
         self.divideDegree=divideDegree
-
-    def __call__(self, lastState, currentState):
+    def __call__(self, lastState,currentState):
         terminal = False
 
         getPositionList=lambda getPos,lastState,currentState:np.linspace(getPos(lastState),getPos(currentState),self.divideDegree,endpoint=True)
@@ -305,6 +268,8 @@ class SampleTrajectory:
 
         while self.isTerminal(state,state):
             state = self.reset()
+            print(L2NormdistanceList)
+            print(np.array(L2NormdistanceList) <= self.minDistance)
 
         trajectory = []
         lastState=state
@@ -334,7 +299,7 @@ class SampleTrajectoryWithRender:
     def __call__(self, policy):
         state = self.reset()
 
-        while self.isTerminal(state, state):
+        while self.isTerminal(state,state):
             state = self.reset()
 
         trajectory = []
@@ -367,6 +332,5 @@ class RewardFunctionCompete():
             reward += self.deathPenalty
 
         return reward
->>>>>>> origin/multiChasingNoPhyscis
 if __name__ == "__main__":
     main()
---
+++ exec/testInference.py
@@ -0,0 +1,2 @@
+from src.inference import IsInferenceTerminal, ObserveStateOnly, InferOneStep, \
+    InferDiscreteChasingAndDrawDemo
