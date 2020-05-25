63e5d02468308308ecf9bd973f0f2d6b1d86adfb
shaozhe <shaozhecheng@outlook.com>
2020-05-26 04:10:47 +0800

    add augment data

--- exec/evaluateSupervisedLearning/3wolves1sheep/trainWolvesThreeCenterControlActionSpace444/trainCenterControlWolvesResNet.py
+++ exec/evaluateSupervisedLearning/3wolves1sheep/trainWolvesThreeCenterControlActionSpace444/trainCenterControlWolvesResNet.py
@@ -150,7 +150,25 @@ def trainOneCondition(manipulatedVariables):
     trajectories = [[filterState(timeStep) for timeStep in trajectory] for trajectory in loadedTrajectories]
     print(len(trajectories))
 
-    preProcessedTrajectories = np.concatenate(preProcessTrajectories(trajectories))
+# augment
+    unaugmentPreProcessedTrajectories = np.concatenate(preProcessTrajectories(trajectories))
+
+    numWolves = 3
+    augmentIdsForWolf = [list(Id) for Id in list(it.permutations(range(1, numWolves + 1)))]
+
+    agumentIdsForState = []
+    for Id in augmentIdsForWolf:
+        augmentId = [0] + Id
+        agumentIdsForState.append(augmentId)
+
+    def augment(timeStep):
+        newTimeSteps = [state[stateId], action[Id], actionDist[Id] for stateId, Id in zip(agumentIdsForState, augmentIdsForWolf)]
+        return newTimeSteps
+
+    preProcessedTrajectories = [agument(timeStep) for timeStep in unaugmentPreProcessedTrajectories]
+###
+    print(len(preProcessedTrajectories))
+    print(preProcessedTrajectories[0])
 
     trainData = [list(varBatch) for varBatch in zip(*preProcessedTrajectories)]
     valuedTrajectories = [addValuesToTrajectory(tra) for tra in trajectories]
